eeac0052fb634b82f5d382ed03dd459d39748cc7
Andrey Artemenko <andrey.artemenko@gmail.com>
2017-05-28 21:19:32 +0300

    Refactored TodoSearch component

--- src/components/TodoSearch.js
+++ src/components/TodoSearch.js
@@ -3,22 +3,26 @@ import {connect}  from 'react-redux';
 import actions from '../actions/actions';
 
 class TodoSearch extends Component {
+    handleSearch = () => {
+        const searchText = this.refs.searchText.value;
+        this.props.dispatch(actions.setSearchText(searchText));
+    };
+
+    handleShowCompleted = () => {
+        this.props.dispatch(actions.toggleShowCompleted());
+    };
+
     render() {
-        const {dispatch, showCompleted, searchText} = this.props;
+        const {showCompleted, searchText} = this.props;
 
         return (
             <div className="container__header">
                 <div>
-                    <input type="search" ref="searchText" placeholder="Search todos" value={searchText} onChange={() => {
-                        var searchText = this.refs.searchText.value;
-                        dispatch(actions.setSearchText(searchText));
-                    }}/>
+                    <input type="search" ref="searchText" placeholder="Search todos" value={searchText} onChange={this.handleSearch}/>
                 </div>
                 <div>
                     <label>
-                        <input type="checkbox" ref="showCompleted" checked={showCompleted} onChange={() => {
-                            dispatch(actions.toggleShowCompleted());
-                        }}/>
+                        <input type="checkbox" ref="showCompleted" checked={showCompleted} onChange={this.handleShowCompleted}/>
                         Show completed todos
                     </label>
                 </div>
